49252fff016529980cdfa18a0201acb94eb84c1b
vudiep411 <vudiep411@gmail.com>
2023-12-22 01:03:17 -0800

    readme template, database schema

--- database/changelog.sql
+++ database/changelog.sql
@@ -2,7 +2,7 @@
 
 
 --changeset vu_diep_1:1
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
