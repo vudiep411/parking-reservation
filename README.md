b0897cd4b87e432e0c6bb77b74d1b2502c156123
vudiep411 <vudiep411@gmail.com>
2023-12-22 00:58:08 -0800

    readme template, database schema

---
+++ README.md
@@ -0,0 +1,14 @@
+# Parking Reservation System Design
+Designed APIs, schema for a parking reservation system.
+
+## Folder Structure
+
+* `database`: Contains database schema in changelog.sql
+* `integration`: Contains Express APIs and settings for nginx API gateway
+* `terraform`: Contains all resources for deployment
+
+## CI/CD
+Github action will deploy the new changes made to the database schema and also run tests for APIs
+
+## Architecture
+<img src="./documents/schema.png"/>
--- database/changelog.sql
+++ database/changelog.sql
@@ -1,2 +1,28 @@
 --liquibase formatted sql
 --changeset vu_diep:1
+CREATE TABLE IF NOT EXISTS user (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15),
+    name VARCHAR(100) NOT NULL
+);
+
+--changeset vu_diep:1
+CREATE TABLE IF NOT EXISTS parking_spot (
+    id VARCHAR(255) PRIMARY KEY,
+    status BOOLEAN,
+    vehicle_type VARCHAR(255),
+    rate DECIMAL(10, 2),
+);
+
+--changeset vu_diep:1
+CREATE TABLE IF NOT EXISTS reservation (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    parking_spot_id VARCHAR(255) NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP  NOT NULL,
+    license_plate VARCHAR(255),
+    status VARCHAR(30)
+);
