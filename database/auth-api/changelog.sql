49f53ffe4205d73b5d70a23856bcc793db2abbd8
vudiep411 <vudiep411@gmail.com>
2023-12-24 17:23:27 -0800

    Update schema

--- database/auth-api/changelog.sql
+++ database/auth-api/changelog.sql
@@ -1,15 +1,18 @@
 --liquibase formatted sql
 
 --changeset vu_diep_1:3
-
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone_number VARCHAR(15),
     password VARCHAR(255),
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    date_created TIMESTAMP
 );
 
+ALTER TABLE users ADD COLUMN date_created TIMESTAMP DEFAULT NOW();
+
+--changeset vu_diep_2:1
 CREATE TABLE IF NOT EXISTS login_record (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
@@ -17,6 +20,7 @@ CREATE TABLE IF NOT EXISTS login_record (
     ip_address VARCHAR(30)
 );
 
+--changeset vu_diep_3:1
 CREATE TABLE refresh_tokens (
   id SERIAL PRIMARY KEY,
   user_id INTEGER,
